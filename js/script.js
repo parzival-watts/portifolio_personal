cb7d5054ed345b0ed0b2bb843ccddd822838d590
parzival-watts <parzival.watts.pw@gmail.com>
2023-11-12 02:28:12 -0300

    feat: text typing animation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log("Start")
+/* ========================  typing animation ========================== */
+
+var typed = new Typed(".typing",{
+    strings:["Web Designer","Web Developer","Mobile Developer","Systems Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+
